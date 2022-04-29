41f37528c59cb43d32879dbd364d16f3bd818fab
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-04-29 20:57:26 +0300

    todo commit

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -2,10 +2,37 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
+   
+        <footer className="footer">
 
-        
-    </div>
+{/* <!-- This should be `0 items left` by default --> */}
+<span className="todo-count">
+    <strong>2</strong>
+    items left
+</span>
+
+<ul className="filters">
+    <li>
+        <a className="selected">All</a>
+    </li>
+    <li>
+        <a>Active</a>
+    </li>
+    <li>
+        <a>Completed</a>
+    </li>
+</ul>
+
+{/* <!-- Hidden if no completed items are left ↓ --> */}
+<button className="clear-completed">
+    Clear completed
+</button>
+</footer>
+
+
+
+
+    
   )
 }
 
