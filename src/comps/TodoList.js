577906cb83ab54cb2b1cc20e7910f3cc350e6e4b
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-04-30 21:55:42 +0300

    commited

--- src/comps/TodoList.js
+++ src/comps/TodoList.js
@@ -4,16 +4,16 @@ import {useState} from 'react'
 function TodoList({todos}) {
  const [isCompleted, setIsCompleted]=useState(false)
 
-const changeClassName= (e)=> {(isCompleted== false ? setIsCompleted(true): setIsCompleted(false))}
+const changeClassName= ()=> {(isCompleted== false ? setIsCompleted(true): setIsCompleted(false))}
   return (
     <div className='main' > 
         
 <input type="checkbox" className='toggle-all' /> <label htmlFor="toggle-all">Mark all as complete</label>
 <ul className='todo-list'>
     {todos.map((todo, index)=>(
-      <li key={index} className={todo.isCompleted ? 'completed' :'' } > 
+      <li key={index} className={isCompleted ? 'completed' :'' } > 
        <div className='view' > 
-        <input type="checkbox" className='toggle' onChange={changeClassName} checked={changeClassName} />
+        <input type="checkbox" className='toggle' onChange={changeClassName} />
        <label > {todo} </label> 
        <button className='destroy' ></button>
         </div>
